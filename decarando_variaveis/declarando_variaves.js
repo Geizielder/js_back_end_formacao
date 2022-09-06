db2b9d9c88e2d9365d2c97cdadcb412737bd4706
geizielder <geizielder.cordeiro@ssinovatec.com.br>
2022-09-06 10:57:26 -0300

    Alterções no README | Aula Let

---
+++ decarando_variaveis/declarando_variaves.js
@@ -0,0 +1,20 @@
+// //var
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// /**------------------- */
+// let forma = 'retângulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// // area = altura * comprimento;// mesmo sem o let essa forma contunua a apresentar o resultado
+// if(forma === 'retângulo') {
+//     area = altura * comprimento;
+// }else {
+//     area = (altura * comprimento) / 2;
+// }
+// console.log(area);
+//let area;//ess tetse gera uma erro descomente e execute para ver o log
